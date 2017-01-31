adcb172cfa43fc88d127a1d9d3341954ea5744af
PayAsian <PayAsian@yahoo.com>
2017-01-31 12:48:08 -0700

    added a model folder and classes to my C++ project

---
+++ FirstProjectForCPlusPlus/Model/Timer.cpp
@@ -0,0 +1,9 @@
+//
+//  Timer.cpp
+//  FirstProjectForCPlusPlus
+//
+//  Created by Trevino, Peyton on 1/31/17.
+//  Copyright © 2017 Trevino, Peyton. All rights reserved.
+//
+
+#include "Timer.hpp"
---
+++ FirstProjectForCPlusPlus/Model/Timer.hpp
@@ -0,0 +1,16 @@
+//
+//  Timer.hpp
+//  FirstProjectForCPlusPlus
+//
+//  Created by Trevino, Peyton on 1/31/17.
+//  Copyright © 2017 Trevino, Peyton. All rights reserved.
+//
+
+#ifndef Timer_hpp
+#define Timer_hpp
+
+#include <time.h>
+
+
+
+#endif /* Timer_hpp */
